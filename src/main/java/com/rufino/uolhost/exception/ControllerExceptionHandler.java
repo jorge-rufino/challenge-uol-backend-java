e7f7d7a3f636ef61629516101e7a37b924346420
jorge-rufino <jorge.rufino1986@gmail.com>
2023-10-30 23:18:53 -0300

    fix: Fixed validation error

--- src/main/java/com/rufino/uolhost/exception/ControllerExceptionHandler.java
+++ src/main/java/com/rufino/uolhost/exception/ControllerExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.rufino.uolhost.exception;
 
 import java.util.NoSuchElementException;
+import java.util.Set;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 
 @RestControllerAdvice
@@ -20,7 +22,24 @@ public class ControllerExceptionHandler {
 	
 	@ExceptionHandler(ConstraintViolationException.class)
 	public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException exception) {
-		ExceptionDto dto = new ExceptionDto("Há campos nulos ou em branco", "400");
+		
+		Set<ConstraintViolation<?>> list = exception.getConstraintViolations();
+		
+		String message = "";		
+		
+		for(ConstraintViolation<?> obj : list) {
+			String field = obj.getPropertyPath().toString();
+			
+			if(field.equals("email")) {
+				message = "Campo email inválido.";
+			}
+			
+			if(field.equals("name")) {
+				message = "Campo nome inválido.";
+			}
+		}
+		
+		ExceptionDto dto = new ExceptionDto(message, "400");
 		
 		return ResponseEntity.badRequest().body(dto);
 	}
